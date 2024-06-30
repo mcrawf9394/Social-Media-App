e2292f3c63e6d70e6f16a367e9cd0b9c0f369a7d
Michael (Sam) Crawford <156837862+mcrawf9394@users.noreply.github.com>
2024-06-30 06:05:12 -0400

    Delete TemplateBackend directory

--- TemplateBackend/tests/Route.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest')
-const express = require('express')
-const app = express()
-const indexRouter = require('../routes/index')
-app.use('/', indexRouter)
-test("this is a test", async () => { 
-    const response = await request(app).get('/')
-    expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
-})
