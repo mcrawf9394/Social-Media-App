7723d5e57f188657bd3d382238d970d284d5bc98
mcrawf9394 <mjc9394@gmail.com>
2024-07-10 07:19:16 -0400

    Added the basic forms for signup and login, and made sure that if a user is not logged in it routes you to the login page.

--- Frontend/src/components/profile/Login.jsx
+++ Frontend/src/components/profile/Login.jsx
@@ -5,7 +5,7 @@ function Login () {
     const [user, setUser] = useState('')
     const [pass, setPass] = useState('')
     return <>
-        <Form className="grid grid-rows-5 h-96 mt-10">
+        <Form className="grid grid-rows-5 h-96 mt-10 w-10/12 mx-auto md:w-8/12">
             <label className="text-gray-400 self-end" htmlFor="username">Username</label>
             <input id="username" className="bg-white border-2 border-solid border-gray-400 h-10" type="text" value={user} onChange={setUser} />
             <label className="text-gray-400 self-end" htmlFor="pass">Password</label>
@@ -15,11 +15,13 @@ function Login () {
 
             }}>Submit</button>
         </Form>
-        <h1 className="text-gray-400">Don't have an account? Sign up!</h1>
-        <button className="text-gray-400" onClick={click => {
-            click.preventDefault()
-            navigate('/signup')
-        }}>Sign up</button>
+        <div className="w-10/12 mx-auto grid grid-rows-2">
+            <h1 className="text-gray-400 text-center">Don't have an account? Sign up!</h1>
+            <button className="text-gray-400 justify-self-center" onClick={click => {
+                click.preventDefault()
+                navigate('/signup')
+            }}>Sign up</button>
+        </div>
     </>
 }
 export default Login
--- Frontend/src/components/profile/Sign-up.jsx
+++ Frontend/src/components/profile/Sign-up.jsx
@@ -1,6 +1,21 @@
+import { Form } from "react-router-dom"
+import { useState } from "react"
 function SignUp () {
+    const [user, setUser] = useState('')
+    const [pass, setPass] = useState('')
+    const [confirm, setConfirm] = useState('')
     return <>
-    
+        <Form className="grid grid-rows-7 h-96 mt-10 w-10/12 mx-auto md:w-8/12">
+            <label className="text-gray-400 self-end" htmlFor="user">Username</label>
+            <input className="bg-white border-2 border-solid border-gray-400 h-10" id="user" type="text" value={user} onChange={e => setUser(e.target.value)} required/>
+            <label className="text-gray-400 self-end" htmlFor="pass">Password</label>
+            <input className="bg-white border-2 border-solid border-gray-400 h-10" id="pass" type="text" value={pass} onChange={e => setPass(e.target.value)} required/>
+            <label className="text-gray-400 self-end" htmlFor="confirm">Confirm Password</label>
+            <input className="bg-white border-2 border-solid border-gray-400 h-10" id="confirm" type="text" value={confirm} onChange={e => setConfirm(e.target.value)} required/>
+            <button className="text-gray-400" onClick={click => {
+                click.preventDefault()
+            }}>Submit</button>
+        </Form>
     </>
 }
 export default SignUp
